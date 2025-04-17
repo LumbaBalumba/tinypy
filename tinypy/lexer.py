69cecb5a47c9480838bad12f45f967180a42fb40
leterDieu <leterdieu@proton.me>
2025-04-17 11:06:22 +0300

    style: flake8

--- tinypy/lexer.py
+++ tinypy/lexer.py
@@ -7,7 +7,10 @@ from .errors import LexerError
 
 reserved = {
     "let": "LET",
-    # future: "if": "IF", "else": "ELSE", "while": "WHILE", "print": "PRINT",
+    "if": "IF",
+    "else": "ELSE",
+    "while": "WHILE",
+    "print": "PRINT",
 }
 
 tokens = [
@@ -23,14 +26,57 @@ tokens = [
     # Parentheses
     "LPAREN",
     "RPAREN",
+    # Braces
+    "LBRACE",
+    "RBRACE",
+    # Brackets
+    "LBRACKET",
+    "RBRACKET",
 ] + list(reserved.values())
 
 # --- Single‑character tokens -------------------------------------------------
 
-# Your code here
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_STAR   = r'\*'
+t_SLASH  = r'/'
+t_ASSIGN = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LBRACE  = r'\{'
+t_RBRACE  = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 
 # --- Multi-character tokens --------------------------------------------------
 
-# Your code here
+t_WHILE = rf'{reserved["while"]}'
+t_PRINT = rf'{reserved["print"]}'
+
+def t_NUMBER(t):
+    r'([0-9]*[\.])?[0-9]+'
+    t.value = float(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore  = ' \t'
+
+def t_IDENT(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'IDENT')
+    return t
+
+def t_error(t):
+    raise LexerError
+    t.lexer.skip(1)
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+t_ignore_COMMENT = r'\#.*'
 
 lexer = lex.lex()
