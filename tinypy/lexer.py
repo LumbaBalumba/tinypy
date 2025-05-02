52d108da64ce77a9ddd3a3868136bf12412aee83
He_Pa3Pa6oT4uk <113712691+HePa3Pa6oT4uk@users.noreply.github.com>
2025-05-02 19:17:26 +0300

    Update lexer.py
    Probably the thing that was needed

--- tinypy/lexer.py
+++ tinypy/lexer.py
@@ -3,7 +3,7 @@ Extend in Lecture 2 (keywords, braces, etc.)
 """
 
 import ply.lex as lex
-from .errors import LexerError
+from errors import LexerError
 
 reserved = {
     "let": "LET",
@@ -27,10 +27,28 @@ tokens = [
 
 # --- Single‑character tokens -------------------------------------------------
 
-# Your code here
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_STAR   = r'\*'
+t_SLASH  = r'/'
+t_ASSIGN = r'\='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 
 # --- Multi-character tokens --------------------------------------------------
 
 # Your code here
+def t_NUMBER(t):
+    r'-?\d+\[.,]\d+?'
+    t.value = float(t.value)
+    return t
+
+def t_IDENT(t):
+    r'[a-zA-Z][a-zA-Z]*'
+    t.type = reserved.get(t.value, 'IDENT')
+    return t
+
+def t_error(t):
+    raise LexerError(f"Invalid token '{t.value}' at line {t.lineno}")
 
 lexer = lex.lex()
